5b6376695ae18bfa5e7fb5b6accfd70ee475e857
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-10-02 17:32:12 -0300

    feat: add skeleton collider on attacking

--- Assets/Scripts/Enemy/AnimationControl.cs
+++ Assets/Scripts/Enemy/AnimationControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class AnimationControl : MonoBehaviour
 {
     private Animator animator;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
 
 
     private void Start()
@@ -15,5 +18,25 @@ public class AnimationControl : MonoBehaviour
     public void PlayAnimation(int animationValue)
     {
         animator.SetInteger("transition", animationValue);
+    }
+
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+        if (hit != null)
+        {
+            //detect player collision
+            Debug.Log("player hit");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
